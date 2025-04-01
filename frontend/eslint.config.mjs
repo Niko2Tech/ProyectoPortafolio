c5a516f4ba69b183ef8ce6835470e3f0fc7fc400
Nicolas Jimenez <niko12121212@gmail.com>
2025-04-01 19:03:05 -0300

    add ESLint and Prettier configurations for code formatting and linting

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  
-)
+export default withNuxt({
+  rules: {
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'stylistic/quotes': ['error', 'single'],
+  },
+})
