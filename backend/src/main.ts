eab004e8b4d89b9ee6df62cf9dcdbd0c126db561
Nicolas Jimenez <niko12121212@gmail.com>
2025-05-19 14:37:53 -0400

    se agrega lo necesario para el uso de jwt

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,9 +13,13 @@ async function bootstrap() {
     .setDescription('Aplicacion de gestion de inventario y ventas')
     .setVersion('1.0')
     .addTag('Api de Ferreteria')
+    .addCookieAuth('access_token', {
+      type: 'http',
+    })
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, documentFactory);
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
