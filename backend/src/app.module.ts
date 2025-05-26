5f9054415c6d94a58bf0b0157ebcaed160a7b1a5
Nicolas Jimenez <niko12121212@gmail.com>
2025-05-26 12:48:57 -0400

    Actualizacion el globals pipes para mas validaciones

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,22 @@ import { UsersModule } from './users/users.module';
 import { RolsModule } from './rols/rols.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { SubcategoriesModule } from './subcategories/subcategories.module';
+import { BrandsModule } from './brands/brands.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, RolsModule, AuthModule, CategoriesModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    RolsModule,
+    AuthModule,
+    CategoriesModule,
+    ProductsModule,
+    SubcategoriesModule,
+    BrandsModule,
+    SuppliersModule,
+  ],
 })
 export class AppModule {}
