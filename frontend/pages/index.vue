ce54adf01ca224c67ac9f96434dfaaa78bae472f
Nicolas Jimenez <niko12121212@gmail.com>
2025-05-28 18:19:57 -0400

    Primeras paginas para pruebas

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,38 +1,3 @@
 <template>
-  <header
-    class="fixed top-0 flex justify-between items-center mx-auto p-4 bg-white shadow-md w-full"
-  >
-    <h1 class="text-2xl font-bold text-gray-800">Proyecto de portafolio</h1>
-    <img src="/img/logo_duoc.webp" alt="logo duoc uc" class="w-64" />
-  </header>
-
-  <main class="h-full w-full flex flex-col items-center justify-center text-center p-6 mt-20">
-    <!--<img src="/img/inventory_system.webp" alt="Gestor de Inventario" class="w-80 mb-4" />-->
-    <h2 class="text-3xl font-bold text-gray-900">Gestor de Inventario y Punto de Venta</h2>
-    <p class="text-lg text-gray-700 max-w-2xl mt-2">
-      Nuestro proyecto es un sistema de gestión de inventario y punto de venta, construido con
-      NestJS en el backend y Nuxt en el frontend.
-    </p>
-    <h3 class="text-2xl my-2 font-semibold">Tecnologias utilizadas</h3>
-    <div class="relative flex flex-col items-center">
-      <img src="/img/javascript.png" alt="JavaScript" class="w-32 mb-4" />
-      <div class="flex space-x-16">
-        <img src="/img/vue.png" alt="Vue.js" class="w-32" />
-        <img src="/img/node.png" alt="Node.js" class="w-32" />
-      </div>
-      <div class="flex justify-between w-full mt-4 px-10 gap-4">
-        <img src="/img/nuxt.svg" alt="Nuxt.js" class="w-32" />
-        <img src="/img/postgres.png" alt="MySQL" class="w-32 mt-6" />
-        <img src="/img/nest.svg" alt="NestJS" class="w-32" />
-      </div>
-    </div>
-  </main>
-  <footer class="fixed bottom-0 w-full bg-gray-900 text-white text-center py-4 mt-4">
-    <p class="">
-      &copy; {{ new Date().getFullYear() }} Proyecto desarrollado por Andrés Quiroz, Nicolás Jiménez
-      y Cristian Matus. Licencia MIT.
-    </p>
-  </footer>
+  <p>index</p>
 </template>
-
-<script setup></script>
---
+++ frontend/pages/login.vue
@@ -0,0 +1,129 @@
+<template>
+  <main class="flex w-full h-screen items-center justify-center">
+    <section class="card bg-base-100 w-96 shadow-sm p-6">
+      <h2 class="text-4xl font-bold text-center mb-4">Iniciar sesión</h2>
+      <form class="space-y-4" @submit.prevent="handleLogin">
+        <!-- Email con ícono -->
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text text-2xl">Correo electrónico</span>
+          </label>
+
+          <label class="input input-bordered flex items-center gap-2">
+            <!-- Ícono de email -->
+            <svg class="h-5 w-5 opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                stroke-width="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+
+            <input
+              id="email"
+              v-model="email"
+              type="email"
+              class="grow"
+              placeholder="mail@ejemplo.com"
+              required
+              title="Ingresa un correo electrónico válido"
+            />
+          </label>
+
+          <p class="text-sm text-red-500 leading-tight" v-if="emailError">
+            Ingrese un correo electrónico válido (ej: usuario@dominio.com)
+          </p>
+        </div>
+
+        <!-- Contraseña con validación -->
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text text-2xl">Contraseña</span>
+          </label>
+
+          <label class="input input-bordered flex items-center gap-2">
+            <!-- Ícono decorativo -->
+            <svg class="h-5 w-5 opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                stroke-width="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path
+                  d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                ></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+
+            <input
+              id="password"
+              v-model="password"
+              :type="viewPassword ? 'text' : 'password'"
+              class="grow"
+              placeholder="*********"
+              required
+              minlength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Debe tener al menos 8 caracteres, incluyendo una mayúscula, una minúscula y un número"
+            />
+          </label>
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <input type="checkbox" class="checkbox checkbox-sm" @click="viewPasswordToggle()" />
+            <span class="label-text">Mostrar contraseña</span>
+          </label>
+        </div>
+
+        <!-- Botón -->
+        <button type="submit" class="btn btn-primary w-full text-xl">Ingresar</button>
+
+        <!-- Mensaje de error -->
+        <p v-if="errorMessage" class="text-error text-sm text-center mt-2">
+          {{ errorMessage }}
+        </p>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user'
+interface LoginForm {
+  email: string
+  password: string
+}
+const email = ref('')
+const emailError = ref(false)
+const password = ref('')
+const errorMessage = ref('')
+const viewPassword = ref(false)
+const userStore = useUserStore()
+
+const viewPasswordToggle = () => {
+  viewPassword.value = !viewPassword.value
+}
+
+const handleLogin = async () => {
+  await userStore.login({
+    email: email.value,
+    password: password.value,
+  })
+  if (userStore.usuario === null) {
+    errorMessage.value =
+      'Credenciales incorrectas. Por favor, verifica tu correo electrónico y contraseña.'
+  } else {
+    errorMessage.value = ''
+    // Redirigir a la página de inicio o dashboard
+    navigateTo('/home')
+  }
+}
+</script>
