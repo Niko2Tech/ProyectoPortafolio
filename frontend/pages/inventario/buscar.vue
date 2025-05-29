6058abd2c497f782f6a48b4ffef2f05df0630211
Nicolas Jimenez <niko12121212@gmail.com>
2025-05-29 19:01:11 -0400

    Agregar páginas de inventario: buscar, crear y editar con estructura básica

---
+++ frontend/pages/inventario/buscar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'internal',
+})
+</script>
+
+<template>
+  <h2>Buscar prodcuto</h2>
+</template>
---
+++ frontend/pages/inventario/editar.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'internal',
+})
+</script>
+<template>
+  <h2>Crear producto</h2>
+</template>
