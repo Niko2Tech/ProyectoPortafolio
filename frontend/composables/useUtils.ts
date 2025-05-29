9754695c415872eab74a7309b8dfc5800731965d
Nicolas Jimenez <niko12121212@gmail.com>
2025-05-29 19:00:11 -0400

    Creacion de composable para utilidades

---
+++ frontend/composables/useUtils.ts
@@ -0,0 +1,8 @@
+export function formatChileanCurrency(amount: number): string {
+  return amount.toLocaleString('es-CL', {
+    style: 'currency',
+    currency: 'CLP',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
